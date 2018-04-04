47f0a4a2df2115627b325e01bee0f6c3e0510455
Erik Cedheim <erik.cedheim@ffcg.se>
2018-04-04 13:31:55 +0200

    Event Hub Service

---
+++ src/TempSoft.CQRS.Demo.EventHub/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Runtime;
+using TempSoft.CQRS.Queries;
+using TempSoft.CQRS.ServiceFabric.Events;
+
+namespace TempSoft.CQRS.Demo.EventHub
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        private static void Main()
+        {
+            try
+            {
+                var registry = new QueryBuilderRegistry();
+                var actorProxyFactory = new ActorProxyFactory();
+                var serviceProxyFactory = new ServiceProxyFactory();
+
+                // The ServiceManifest.XML file defines one or more service type names.
+                // Registering a service maps a service type name to a .NET type.
+                // When Service Fabric creates an instance of this service type,
+                // an instance of the class is created in this host process.
+
+                ServiceRuntime.RegisterServiceAsync("EventHubServiceType",
+                    context => new EventBusService(context, registry, serviceProxyFactory, actorProxyFactory)).GetAwaiter().GetResult();
+
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(EventBusService).Name);
+
+                // Prevents this host process from terminating so services keep running.
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
